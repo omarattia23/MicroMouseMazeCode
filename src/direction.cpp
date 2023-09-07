9e35a262e34ae708129ae574ce69a73ed605181a
minagerges2310 <78907071+minagerges2310@users.noreply.github.com>
2023-09-07 06:56:19 +0300

    Update direction.cpp

--- src/direction.cpp
+++ src/direction.cpp
@@ -1,7 +1,24 @@
-int IN1,IN2,IN3,IN4,En1,En2;
 #include <Arduino.h>
 
-void forward(void)
+#define IN1  4
+#define IN2 32
+#define IN3 18
+#define IN4 19
+#define En1 20
+#define En2 21
+
+void directionInit(void)
+{
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  pinMode(En1, OUTPUT);
+  pinMode(En2, OUTPUT);
+
+}
+
+void backward(void)
 {
  analogWrite(IN1, 255);
  analogWrite(IN2, 0);
@@ -9,12 +26,12 @@ void forward(void)
  analogWrite(IN4, 0);
 }
 
-void backward(void)
+void forward(void)
 {
  analogWrite(IN1, 0);
- analogWrite(IN2, 255);
+ analogWrite(IN2, 170);
  analogWrite(IN3, 0);
- analogWrite(IN4, 255);
+ analogWrite(IN4, 170);
 }
 
 void right(void)
@@ -29,6 +46,14 @@ void left(void)
  analogWrite(IN1, 0);
  analogWrite(IN2, 255);
  analogWrite(IN3, 255);
+ analogWrite(IN4, 0);
+}
+
+void stop(void)
+{
+ analogWrite(IN1, 0);
+ analogWrite(IN2, 0);
+ analogWrite(IN3, 0);
  analogWrite(IN4, 0);
 }
 void rotate180(void)
